d3ea8ca7dadbd50ea5c636f678f61b209c248ae2
Rafael Scoralick <rafaelscoralick@gmail.com>
2025-01-17 16:06:21 -0300

    prismic

--- src/prismic.ts
+++
@@ -1,7 +0,0 @@
-import * as prismic from '@prismicio/client';
-
-const repositoryName = 'deborapascotto';
-
-export const client = prismic.createClient(`https://${repositoryName}.cdn.prismic.io/api/v2`, {
-  accessToken: 'MC5aMlNQMGhBQUFDUUE4VnJy.YQ49Su-_vRTvv73vv73vv73vv70AZmslNGkS77-9cRzvv71O77-9QxZL77-977-9HBnvv70u', 
-});
